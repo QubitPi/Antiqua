1ec2fb681d0a4becd44a0c7a06b8936c98f8f090
Jack <jack20220723@gmail.com>
2024-09-14 11:06:49 +0800

    Use custom YAML & Markdown validation rules

--- markdownlint.rb
+++ markdownlint.rb
@@ -1,2 +1,20 @@
-rules "~MD002", "~MD003", "~MD005", "~MD007", "~MD013", "~MD022", "~MD024", "~MD029", "~MD033", "~MD034", "~MD036", "~MD041", "~MD055", "~MD057"
-style "#{File.dirname(__FILE__)}/markdownlint.rb"
+# Copyright Jiaqi Liu
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+all
+rule 'MD003', style: :setext_with_atx
+rule 'MD004', style: :sublist
+rule 'MD013', line_length: 120
+rule 'MD029', style: :ordered
+rule 'MD026', punctuation: ".,;:!?"
