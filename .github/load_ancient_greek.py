c510beafb168c7bf98f66e89e3ac7dcda3121162
Jack <jack20220723@gmail.com>
2024-10-12 18:52:55 +0800

    Update to use to new loader

--- .github/load_ancient_greek.py
+++ .github/load_ancient_greek.py
@@ -1,4 +1,4 @@
-from wilhelm_python_sdk.ancient_greek_neo4j_loader import load_into_database
+from wilhelm_python_sdk.ancient_greek_loader import load_into_database
 
 if __name__ == "__main__":
     load_into_database("ancient-greek.yaml")
--- .github/load_german.py
+++ .github/load_german.py
@@ -1,4 +1,4 @@
-from wilhelm_python_sdk.german_neo4j_loader import load_into_database
+from wilhelm_python_sdk.german_loader import load_into_database
 
 if __name__ == "__main__":
     load_into_database("german.yaml")
